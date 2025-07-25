fb3c811e5eb8bfe990f67b5e38d038036fafa66d
Ekram <ekru6482@gmail.com>
2025-07-25 11:36:22 +0300

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -13,3 +13,15 @@ for(let n=2;n<10;n++){
         }
     
 }
+// In another way 
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {     
+
+  for (let j = 2; j < i; j++) {    
+    if (i % j == 0) continue nextPrime; 
+  }
+
+  alert(i);  
+}
