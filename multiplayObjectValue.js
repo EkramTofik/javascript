ad01a1ab81c72a0e8b54cdc986ff82ae9120df27
Ekram <ekru6482@gmail.com>
2025-09-19 21:30:52 +0300

    Update multiplayObjectValue.js

--- multiplayObjectValue.js
+++ multiplayObjectValue.js
@@ -1,14 +1,10 @@
-function multiplayNumeric(obj){
-            let newObj={};
+//without returning a new object 
+let multiplayNumeric=function (obj){
     for (let key in obj){
         if(typeof obj[key] === "number"){            
-            newObj[key]=obj[key]*2;
+            obj[key]*=2;
         }
-        else{
-            newObj[key]=obj[key];
-        }
-    }        
-console.log(newObj);
+    }
 };
 let menu= {
     width:200,
@@ -16,9 +12,29 @@ let menu= {
     title:"My Menu"
 };
 multiplayNumeric(menu);
-let menu1= {
-    width:300,
-    height:600,
-    title:"My Menu"
-};
-multiplayNumeric(menu1);
+console.log(menu);
+// With Returning an Object 
+// function multiplayNumeric(obj){
+//             let newObj={};
+//     for (let key in obj){
+//         if(typeof obj[key] === "number"){            
+//             newObj[key]=obj[key]*2;
+//         }
+//         else{
+//             newObj[key]=obj[key];
+//         }
+//     }        
+// console.log(newObj);
+// };
+// let menu= {
+//     width:200,
+//     height:300,
+//     title:"My Menu"
+// };
+// multiplayNumeric(menu);
+// let menu1= {
+//     width:300,
+//     height:600,
+//     title:"My Menu"
+// };
+// multiplayNumeric(menu1);
