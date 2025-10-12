ac34292b856ff65f17042985ea6f28e5f84ed011
Ekram <ekru6482@gmail.com>
2025-10-12 14:58:08 +0300

    Update sortInDecreasing.js

--- sortInDecreasing.js
+++ sortInDecreasing.js
@@ -6,3 +6,9 @@ function compare(a, b) {
 let arr=[5,8,2,-7,9];
 arr.sort(compare);
 console.log(arr);
+//OR SIMPLY 
+let arr1 = [5, 2, 1, -10, 8];
+
+arr1.sort((a, b) => b - a);
+
+console.log(arr1);
