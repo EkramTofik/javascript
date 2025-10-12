05d9ba2aba6bbf652c0f811de6c6f3ea98c6a4c6
Ekram <ekru6482@gmail.com>
2025-10-12 14:54:59 +0300

    Create sortInDecreasing.js

---
+++ sortInDecreasing.js
@@ -0,0 +1,8 @@
+function compare(a, b) {
+  if (a > b) return -1; // if the first value is greater than the second
+  if (a == b) return 0; // if values are equal
+  if (a < b) return 1; // if the first value is less than the second
+}
+let arr=[5,8,2,-7,9];
+arr.sort(compare);
+console.log(arr);
