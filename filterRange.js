b8298d863665f286d3bd29ba830d91c90dd05d71
Ekram <ekru6482@gmail.com>
2025-10-12 14:24:40 +0300

    Update filterRange.js

--- filterRange.js
+++ filterRange.js
@@ -1,8 +1,6 @@
 let arr=[0,5,3,8,1];
-let value=[];
 function filterRange(arr,x,y){
-value=(arr.filter(item=>item>=x && item<=y));
-return value;
+return(arr.filter(item=>item>=x && item<=y));
 }
 console.log(arr);
 console.log(filterRange(arr,0,3));
