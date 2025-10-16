d66b24e019c74177ee54ac527e99bb72c8bbe2e0
Ekram <ekru6482@gmail.com>
2025-10-16 22:51:57 +0300

    Create arrayMapToObject.js

---
+++ arrayMapToObject.js
@@ -0,0 +1,30 @@
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(item=>{
+    return{
+        fullname:item.name+" " +item.surname,
+    id: item.id
+}
+    }
+);
+//OR
+// let usersMapped = users.map(user => ({
+//   fullName: `${user.name} ${user.surname}`,
+//   id: user.id
+// }));
+/* OUT PUT SHOULD BE
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+console.log(usersMapped);
+
+console.log(usersMapped[0].id);
+console.log(usersMapped[2].fullname );
+
